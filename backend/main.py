744062771e43915ee6662eb22e077a8b910d6da2
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2022-01-03 01:53:06 +0530

    add route to get level data

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 import sqlite3
 
 from .data import initialize
@@ -9,6 +9,23 @@ initialize(db_connection)  # create and hydrate database if not already done
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/levels/{level_id}")
+async def root(level_id: int, response: Response):
+    try:
+        maybe_level = db_connection.execute(
+            'SELECT * FROM levels where id=(?)',
+            (level_id,)
+        ).fetchone()
+
+        if maybe_level is not None:
+            return {
+                'id': maybe_level[0],
+                'title': maybe_level[1],
+                'description': maybe_level[2],
+                'question': maybe_level[3].split(',')
+            }
+    except Exception as e:
+        print(e)
+
+    response.status_code = status.HTTP_404_NOT_FOUND
+    return {}
