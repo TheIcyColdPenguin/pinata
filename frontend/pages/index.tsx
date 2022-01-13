dc09ca12f580c9386e1a66e2415141bf43da4458
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2022-01-13 14:50:28 +0530

    fixed home page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { GetStaticProps } from 'next';
 
-import { FC, useState } from 'react';
+import { FC, MouseEvent as ReactMouseEvent, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import styles from '../styles/Home.module.css';
@@ -9,36 +9,36 @@ const Home: FC<{ levels: string[] }> = ({ levels }) => {
     const [selectedLevel, setSelectedLevel] = useState(0);
     const router = useRouter();
 
-    const goToLevel = e => {
+    const goToLevel = (e: ReactMouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
+        router.push(`/levels/${selectedLevel}`);
     };
 
     return (
         <div className={styles['grid']}>
             <div className={styles['title']}>SQL Injection</div>
             <div>sql injection is cool or smth</div>
-
-            <form className={styles}>
-                <select
-                    name="level"
-                    defaultValue={selectedLevel}
-                    onChange={e => {
-                        setSelectedLevel(Number.parseInt(e.target.value));
-                    }}
-                >
-                    {levels.map((level, i) => (
-                        <option value={i} key={i}>
-                            {level}
-                        </option>
-                    ))}
-                </select>
-                <button type="submit" onClick={goToLevel}>
-                    Attempt level{' '}
-                    <pre>
-                        <code>{levels[selectedLevel]}</code>
-                    </pre>
-                </button>
-            </form>
+            <div>
+                <form className={styles['level-picker']}>
+                    <select
+                        name="level"
+                        defaultValue={selectedLevel}
+                        onChange={e => {
+                            setSelectedLevel(Number.parseInt(e.target.value));
+                        }}
+                    >
+                        {levels.map((level, i) => (
+                            <option value={i} key={i}>
+                                {level}
+                            </option>
+                        ))}
+                    </select>
+                    <button type="submit"  onClick={goToLevel}>
+                        Attempt level
+                    </button>
+                </form>
+            </div>
+            <div></div>
         </div>
     );
 };
